1f8bdfcc1638c0a27dfab47e3c0a44f2d88e5e7f
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
-var reshape = require('reshape');
-var sugarml = require('sugarml');
+var reshape = require('reshape')
+var sugarml = require('sugarml')
 
-exports.name = 'sugarml';
-exports.inputFormats = ['sml'];
-exports.outputFormat = 'html';
+exports.name = 'sugarml'
+exports.inputFormats = ['sml']
+exports.outputFormat = 'html'
 
 exports.renderAsync = function (str, options, locals) {
-  options.parser = sugarml;
+  options.parser = sugarml
   if (locals) {console.error('jstransformer-sugarml does not support locals. Use jstransformer-reshape with sugarml as an input instead')}
   return reshape(options)
     .process(str)
-    .then(result => result.output());
-};
+    .then(result => result.output())
+}
