5dbd49a595f900dd24aa712adc616115c1de96f4
Caleb Eby <caleb.eby01@gmail.com>
2016-12-09 12:11:25 -0800

    Distribute line

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ exports.outputFormat = 'html'
 
 exports.renderAsync = function (str, options, locals) {
   options.parser = sugarml
-  if (locals) {console.error('jstransformer-sugarml does not support locals. Use jstransformer-reshape with sugarml as an input instead')}
+  if (locals) {
+    console.error('jstransformer-sugarml does not support locals. Use jstransformer-reshape with sugarml as an input instead')
+  }
   return reshape(options)
     .process(str)
     .then(result => result.output())
